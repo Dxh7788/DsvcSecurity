7a6df372995300baf94a3bf3c3220642c6beed00
Administrator <Administrator@192.168.1.104>
2017-11-20 07:48:17 +0800

    issue #2 Objects工具类研究

--- src/com/CollectionTest.java
+++ src/com/CollectionTest.java
@@ -1,9 +1,7 @@
 package com;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 集合的测试
@@ -15,24 +13,53 @@ public class CollectionTest {
 		 * 测试Iterator和ListIterator
 		 * 任何集合类都能使用Iterator,但是只有List、ArrayList、LinkedList和Vector可以使用ListIterator
 		 * **/
-		List<String> ids = new ArrayList<>(0);
-		ids.add("ddd");
-		ids.add("ddx");
-		ids.add("ddh");
-		
-		ListIterator<String> liter = ids.listIterator();
-		liter.hasNext();
-		String temps = liter.next();
-		System.out.println(temps);
-		liter.set("sdf");
-		liter.add("235");
-		liter.hasPrevious();
-		liter.previous();
-		liter.add("235");
-		liter.nextIndex();
-		System.out.println(Pattern.compile("\\b([\\w]*)\\b").matcher(ids.toString()).find()); 
-		System.out.println(ids.toString().replaceAll("\\b([\\w]*)\\b", "'$1").replaceAll(" ", "").replace("[", "(").replace("]", ")"));
+//		List<String> ids = new ArrayList<>(0);
+//		ids.add("ddd");
+//		ids.add("ddx");
+//		ids.add("ddh");
+//		
+//		ListIterator<String> liter = ids.listIterator();
+//		liter.hasNext();
+//		String temps = liter.next();
+//		System.out.println(temps);
+//		liter.set("sdf");
+//		liter.add("235");
+//		liter.hasPrevious();
+//		liter.previous();
+//		liter.add("235");
+//		liter.nextIndex();
+//		System.out.println(Pattern.compile("\\b([\\w]*)\\b").matcher(ids.toString()).find()); 
+//		System.out.println(ids.toString().replaceAll("\\b([\\w]*)\\b", "'$1").replaceAll(" ", "").replace("[", "(").replace("]", ")"));
 //		System.out.println(s);
 //		System.out.println(ids.toString().replace(",", "','").replace("[", "['").replace("]", "']").replaceAll(" ", ""));
+		
+		/**
+		 * HashMap源码分析
+		 * */
+		Map<String,Object> map =  new HashMap<String,Object>();
+		Map<String,Object> map2 =  new HashMap<String,Object>();
+		map.put("ft", map);
+		map.put("ft2", "123");
+		System.out.println(map.toString());
+		if (map.equals(map2)) {
+			System.out.println("test.....");
+		} 
+		/**
+		 * Map的以下方法就不一一展示了,日常常用
+		 * size();
+		 * isEmpty();
+		 * containsKey(key);
+		 * containsValue(value);
+		 * get(key);
+		 * put(key,value);
+		 * remove(Object);
+		 * putAll(Map);
+		 * clear();
+		 * keySet();
+		 * values();
+		 * EntrySet<K,V>();
+		 * equals();
+		 * hasCode();
+		 */
 	}
 }
---
+++ src/com/ObjectsUtilTest.java
@@ -0,0 +1,48 @@
+package com;
+
+import java.util.Objects;
+
+import com.dsvc.model.Address;
+import com.dsvc.model.User;
+
+/**
+ * 研究下Objects工具类
+ * */
+public class ObjectsUtilTest {
+	public static void main(String[] args) {
+		
+		String t1 = "abc";
+		String t2 = "abc";
+		//Objects.equals和Objects.deepEquals的相同点
+		System.out.println(Objects.equals(t1, t2));
+		System.out.println(Objects.deepEquals(t1, t2));
+		//Objects.equals和Objects.deepEquals的不同点是,deepEquals可用来比较数组
+		String[] ta1={"a","b","c"};
+		String[] ta2={"a","b","c"};
+		System.out.println(Objects.equals(ta1, ta2));
+		System.out.println(Objects.deepEquals(ta1, ta2));
+		//那么自定义类可以这样比较吗?由结果可知无论是Objects.equals或者Objects.deepEquals都无法比较自定义类,因为自定义类无法一一重载hashCode和equals方法
+		User u1 =new User();
+		User u2 =new User();
+		System.out.println(Objects.equals(u1, u2));
+		System.out.println(Objects.deepEquals(u1, u2));
+		//关于hashCode和hash方法,直接调用hashCode和使用Objects的hashCode是一样的.hash只能用来操作数组,不能用来操作非数组
+		System.out.println(t1.hashCode()+"   "+Objects.hashCode(t1));
+		System.out.println(ta1.hashCode()+"   "+Objects.hashCode(ta1));
+		System.out.println(t1.hashCode()+"   "+Objects.hash(t1));
+		
+		u1.setName("ddd");
+		u2.setName("ddd");
+		Address addr = new Address();
+		u1.setAddress(addr);
+		u2.setAddress(addr);
+		u1.setPwd("123");
+		u2.setPwd("234");
+		//hash的行为
+		Objects.hash(u1);
+		System.out.println(Objects.hash(u1.getAddress(),u1.getName(),u1.getPwd()));
+		System.out.println(Objects.hash(u2.getAddress(),u2.getName(),u2.getPwd()));
+		System.out.println(Objects.hash(u1)==Objects.hash(u2));
+		
+	}
+}
